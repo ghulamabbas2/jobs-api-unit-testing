f2beda9a2b04684918d946e6fc5dc66cdaf8ab7e
Ghulam Abbas <ghulamabbas258@gmail.com>
2022-11-17 23:37:57 +0500

    initial jobs api

--- models/users.js
+++ models/users.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,26 +21,5 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
-
-// Encypting passwords before saving
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) {
-//     next();
-//   }
-
-//   this.password = await bcrypt.hash(this.password, 10);
-// });
-
-// Return JSON Web Token
-// userSchema.methods.getJwtToken = function () {
-//   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRES_TIME,
-//   });
-// };
-
-// Compare user password in database password
-// userSchema.methods.comparePassword = async function (enterPassword) {
-//   return await bcrypt.compare(enterPassword, this.password);
-// };
 
 export default mongoose.model("User", userSchema);
